4aa5df0442bbbd4a08451e61c0e9dbd15a603f52
cbott <cbott6@gmail.com>
2014-12-04 22:59:14 -0500

    made rpi i2c communications accomodate sections of bytes

--- rpi/rpi.ino
+++ rpi/rpi.ino
@@ -1,7 +1,10 @@
 #include <Wire.h>
 
 #define SLAVE_ADDRESS 0x04
-int number = 0;
+
+static const byte DATA_BITS = 4;//bits used as data (8-id bits)
+
+int number;
 int state = 0;
 void setup() {
     pinMode(13, OUTPUT);
@@ -17,30 +20,33 @@ void setup() {
 }
  
 void loop() {
-    delay(100);
+    delay(10);
 }
 
 // callback for received data
 void receiveData(int byteCount){
-
     while(Wire.available()) {
         number = Wire.read();
-        Serial.print("data received: ");
-        Serial.println(number);
 
         if (number == 1){
  
             if (state == 0){
-
-                digitalWrite(13, HIGH); // set the LED on
+                digitalWrite(13, LOW); // set the LED on
                 state = 1;
             }
             else{
-                digitalWrite(13, LOW); // set the LED off
+                digitalWrite(13, HIGH); // set the LED off
                 state = 0;
             }
          }
      }
+     number = byte(number);
+     Serial.print("data received: ");
+     Serial.print(number);
+     Serial.print("     ID:");
+     Serial.print((number&byte((256-pow(2,DATA_BITS)))) >> DATA_BITS);//read only the first bits(The id)
+     Serial.print("     Data:");
+     Serial.println(number & byte((pow(2,DATA_BITS)-1)));
 }
 
 // callback for sending data
