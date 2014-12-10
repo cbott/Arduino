f87dd1cc9772dc852e382f4e8a613fcf584edb02
cbott <cbott6@gmail.com>
2014-12-09 20:06:58 -0500

    latest code to receive data from pi

--- rpi/rpi.ino
+++ rpi/rpi.ino
@@ -4,10 +4,24 @@
 
 static const byte DATA_BITS = 4;//bits used as data (8-id bits)
 
-int number;
+int RED_PIN = 8;
+int GREEN_PIN = 9;
+int BLUE_PIN = 10;
+boolean lights[] = {0,0,0};//store rgb value
+
+int number;//value received over i2c
+int id;    //first bits in number ( 11110000 )
+int data;  //later bits in number ( 00001111 )
+
 int state = 0;
 void setup() {
-    pinMode(13, OUTPUT);
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
+    pinMode(BLUE_PIN, OUTPUT);
+    digitalWrite(RED_PIN, HIGH);
+    digitalWrite(GREEN_PIN, HIGH);
+    digitalWrite(BLUE_PIN, HIGH);
+    
     Serial.begin(9600);         // start serial for output
     // initialize i2c as slave
     Wire.begin(SLAVE_ADDRESS);
@@ -20,33 +34,48 @@ void setup() {
 }
  
 void loop() {
-    delay(10);
+  delay(10);
+  if (lights[0]){
+    digitalWrite(RED_PIN, LOW); 
+  }else{
+    digitalWrite(RED_PIN, HIGH); 
+  }
+  if (lights[1]){
+    digitalWrite(GREEN_PIN, LOW);
+  }else{
+    digitalWrite(GREEN_PIN, HIGH);
+  }
+  if (lights[2]){
+    digitalWrite(BLUE_PIN, LOW);
+  }else{
+    digitalWrite(BLUE_PIN, HIGH);
+  }
+    
 }
 
 // callback for received data
 void receiveData(int byteCount){
-    while(Wire.available()) {
+     while(Wire.available()) {
         number = Wire.read();
-
-        if (number == 1){
- 
-            if (state == 0){
-                digitalWrite(13, LOW); // set the LED on
-                state = 1;
-            }
-            else{
-                digitalWrite(13, HIGH); // set the LED off
-                state = 0;
-            }
-         }
      }
+     
      number = byte(number);
+     id = (number&byte((256-pow(2,DATA_BITS)))) >> DATA_BITS;
+     data = number & byte((pow(2,DATA_BITS)-1));
      Serial.print("data received: ");
      Serial.print(number);
      Serial.print("     ID:");
-     Serial.print((number&byte((256-pow(2,DATA_BITS)))) >> DATA_BITS);//read only the first bits(The id)
+     Serial.print(id);//read only the first bits(The id)
      Serial.print("     Data:");
-     Serial.println(number & byte((pow(2,DATA_BITS)-1)));
+     Serial.println(data);
+     
+     if (id == 1){
+       lights[0] = data;  
+     }else if (id == 2){
+       lights[1] = data; 
+     }else if (id == 3){
+       lights[2] = data; 
+     }
 }
 
 // callback for sending data
