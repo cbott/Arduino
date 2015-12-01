052b9a84fd4de75fb0fbb1d6100c34505cbe5189
cbott <cbott6@gmail.com>
2015-11-30 22:03:09 -0500

    made ir timer work

--- IR_Timer/IR_Timer.ino
+++ IR_Timer/IR_Timer.ino
@@ -1,26 +1,44 @@
-const int EMIT_PIN = 10;
-const int DETECT_PIN = A0;
+const int THRESHHOLD = 60;
+
+const int START_PIN = A0;
+const int END_PIN = A1;
+
+unsigned long starttime = 0;
+unsigned long endtime = 0;
+bool timing = false;
 
 void setup() {
-  pinMode(EMIT_PIN, OUTPUT);
-  
   Serial.begin(9600);
-  analogWrite(EMIT_PIN, 150);
 }
 
 void loop() {
-  int total = 0;
-  int readings;
-  for(readings=0; readings < 3; readings++){
-    total += analogRead(DETECT_PIN);
-    delay(50);
-  }
-  int val = int(total / readings);
+  int read1 = analogRead(START_PIN);
+  int read2 = analogRead(END_PIN);
   /*
   for(int i=0; i < val - 30; i++){
     Serial.print("|");
   }*/
-  Serial.println(val);
-  //delay(100);
-
+  /*
+  Serial.print("Detector 1: ");
+  Serial.print(read1);
+  Serial.print("  Detector 2: ");
+  Serial.println(read2);
+  */
+  
+  if(read1 < THRESHHOLD && !timing){
+    starttime = millis();
+    Serial.println("Starting!!!");
+    timing = true;
+  }
+  else if(read2 < THRESHHOLD && timing){
+    endtime = millis();
+    Serial.print("FINISH: ");
+    Serial.print(endtime - starttime);
+    Serial.println("ms");
+    timing = false;
+  }
+  if(timing)
+    Serial.println(millis()-starttime);
+  
+  delay(5);
 }
