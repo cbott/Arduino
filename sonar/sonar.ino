f66ae00f06a0a642a02d89635c18fe9e291e4114
cbott <cbott6@gmail.com>
2016-08-20 14:13:40 -0400

    update sonar for new hardware

--- sonar/sonar.ino
+++ sonar/sonar.ino
@@ -1,6 +1,6 @@
 // include the library code:
 #include <LiquidCrystal.h>
-
+#include <Wire.h>
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
@@ -11,18 +11,42 @@ void setup()
   lcd.begin(20, 4);
   // initialize serial communications
   Serial.begin(9600); 
+  Wire.begin(); 
 }
 
-void loop()
-{
-  int sensor, inches, x;
-  
-  // read the analog output of the EZ1 from analog input 0
-  sensor = analogRead(0);
-  
-  // convert the sensor reading to inches
-  inches = sensor / 2;
-  
+int reading = 0;
+
+void loop() {
+  // step 1: instruct sensor to read echoes
+  Wire.beginTransmission(112); // transmit to device #112 (0x70)
+  // the address specified in the datasheet is 224 (0xE0)
+  // but i2c adressing uses the high 7 bits so it's 112
+  Wire.write(byte(0x00));      // sets register pointer to the command register (0x00)
+  Wire.write(byte(0x50));      // command sensor to measure in "inches" (0x50)
+  // use 0x51 for centimeters
+  // use 0x52 for ping microseconds
+  Wire.endTransmission();      // stop transmitting
+
+  // step 2: wait for readings to happen
+  delay(70);                   // datasheet suggests at least 65 milliseconds
+
+  // step 3: instruct sensor to return a particular echo reading
+  Wire.beginTransmission(112); // transmit to device #112
+  Wire.write(byte(0x02));      // sets register pointer to echo #1 register (0x02)
+  Wire.endTransmission();      // stop transmitting
+
+  // step 4: request reading from sensor
+  Wire.requestFrom(112, 2);    // request 2 bytes from slave device #112
+
+  // step 5: receive reading from sensor
+  if (2 <= Wire.available()) { // if two bytes were received
+    reading = Wire.read();  // receive high byte (overwrites previous reading)
+    reading = reading << 8;    // shift high byte to be high 8 bits
+    reading |= Wire.read(); // receive low byte as lower 8 bits
+    Serial.println(reading);   // print the reading
+  }
+
+/*  
   // print out the decimal result
   Serial.print(inches,DEC);
   
@@ -33,12 +57,13 @@ void loop()
     Serial.print(".");
   }
   Serial.println("|");
+*/
 
   lcd.setCursor(0,0);
   lcd.print("    ");
   lcd.setCursor(0,0);
-  lcd.print(inches);
+  lcd.print(reading);
   // pause before taking the next reading
-  delay(100);                     
+  delay(200);                     
 }
 
